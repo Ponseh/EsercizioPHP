6346a62b6286d9474e9ba277577407e81272d5fc
Luca <80454841+Ponseh@users.noreply.github.com>
2022-04-21 23:23:00 +0200

    Update deleteTeacherConfirmation.php

--- deleteTeacher/deleteTeacherConfirmation.php
+++ deleteTeacher/deleteTeacherConfirmation.php
@@ -1,20 +1,19 @@
+<?php
+    session_start();
+
+    if(isset($_POST["no"])) {
+        header("Location: ..\index.html");
+    } else if(isset($_POST["yes"])) {
+        header("Location: deleteTeacher.php");
+    } else {
+        $_SESSION['Nome_Insegnante'] = $_POST['Nome_Insegnante'];
+        $_SESSION['Cognome_Insegnante'] = $_POST['Cognome_Insegnante'];
+    }
+?>
 <!DOCTYPE html>
 <html lang="it" dir="ltr">
     <head>
         <meta charset="utf-8">
-        <title></title>
-        <?php
-            session_start();
-
-            if(isset($_POST["no"])){
-                header("Location: ..\index.html");
-            } else if(isset($_POST["yes"])){
-                header("Location: deleteTeacher.php");
-            } else {
-                $_SESSION['Nome_Insegnante'] = $_POST['Nome_Insegnante'];
-                $_SESSION['Cognome_Insegnante'] = $_POST['Cognome_Insegnante'];
-            }
-        ?>
     </head>
     <body>
         <h1 style="color:red;">Sei sicuro?</h1>
